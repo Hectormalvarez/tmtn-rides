ac469ab932beff15699feeac0722b0750f60044f
hadev <halvarez@taylormadetech.net>
2022-09-18 06:57:30 -0500

    added trip detail view

--- server/trips/urls.py
+++ server/trips/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from .views import TripView
 
 
-app_name = 'rides'
+app_name = "rides"
 
 urlpatterns = [
-    path('', TripView.as_view({'get': 'list'}), name="trip_list"),
+    path("", TripView.as_view({"get": "list"}), name="trip_list"),
+    path("<uuid:trip_id>/", TripView.as_view({"get": "retrieve"}), name="trip_detail"),
 ]
