a2ffa96762d37e6d7510846c209a22f13d3fceff
hadev <halvarez@taylormadetech.net>
2022-09-18 07:10:25 -0500

    added channels consumers

--- server/rides/routing.py
+++ server/rides/routing.py
@@ -1,9 +1,20 @@
 from django.core.asgi import get_asgi_application
+from django.urls import path
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from rides.middleware import TokenAuthMiddleWareStack
+from trips.consumers import TaxiConsumer
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
+        "websocket": TokenAuthMiddleWareStack(
+            URLRouter(
+                [
+                    path("rides/", TaxiConsumer.as_asgi()),
+                ]
+            ),
+        ),
     }
 )
