ee24ff9b7b33a18b398f81977d65ce3b85e60e36
hadev <halvarez@taylormadetech.net>
2022-09-18 08:22:56 -0500

    added test_join_driver_pool test

--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -18,17 +18,15 @@ TEST_CHANNEL_LAYERS = {
 @database_sync_to_async
 def create_user(username, password, group="rider"):
     # create user
-    user = get_user_model().objects.create_user(
-        username=username, password=password
-    )
+    user = get_user_model().objects.create_user(username=username, password=password)
     # create user group
     user_group, _ = Group.objects.get_or_create(name=group)
     user.groups.add(user_group)
     user.save()
-    
+
     # create access token
     access = AccessToken.for_user(user)
-    
+
     return user, access
 
 
@@ -83,4 +81,21 @@ class TestWebSocket:
         communicator = WebsocketCommunicator(application=application, path="/rides/")
         connected, _ = await communicator.connect()
         assert connected is False
+        await communicator.disconnect()
+
+    async def test_join_driver_pool(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user("test.user@example.com", "p@ssw0rd", "driver")
+        communicator = WebsocketCommunicator(
+            application=application, path=f"/rides/?token={access}"
+        )
+        await communicator.connect()
+        message = {
+            "type": "echo.message",
+            "data": "This is a test message.",
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send("drivers", message=message)
+        response = await communicator.receive_json_from()
+        assert response == message
         await communicator.disconnect()
