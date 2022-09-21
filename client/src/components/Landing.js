11f0554e0dff43705d3056701f24ad2d1488e33d
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-09-20 19:41:17 -0500

    moved landing to components

---
+++ client/src/components/Landing.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, ButtonGroup } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Landing (props) {
+  return (
+    <div className="middle-center">
+      <h1 className="landing logo">Rides</h1>
+      <ButtonGroup>
+        <LinkContainer to="/sign-up">
+          <Button>Sign up</Button>
+        </LinkContainer>
+        <LinkContainer to="/log-in">
+          <Button>Log in</Button>
+        </LinkContainer>
+      </ButtonGroup>
+    </div>
+  );
+};
+
+export default Landing;
