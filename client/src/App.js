57dd413a698e969edaeed3e36cf43f573853054c
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-09-21 20:38:33 -0500

    refactored app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import { LinkContainer } from "react-router-bootstrap";
-import { Outlet, Route, Routes } from "react-router-dom";
+import {
+  Button, Container, Form, Navbar
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
 import Landing from "./components/Landing";
 import SignUp from "./components/SignUp";
@@ -12,18 +13,24 @@ import LogIn from "./components/LogIn";
 import "./App.css";
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const logIn = (username, password) => {
+    setLoggedIn(true)
+    console.log("LOGGIN")
+  }
+
   return (
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout isLoggedIn={isLoggedIn} />}>
           <Route index element={<Landing />} />
           <Route path="sign-up" element={<SignUp />} />
-          <Route path="log-in" element={<LogIn />} />
+          <Route path="log-in" element={<LogIn logIn={logIn} />} />
         </Route>
       </Routes>
   );
 }
 
-function Layout() {
+function Layout({isLoggedIn}) {
   return (
     <>
       <Navbar bg="light" expand="lg" variant="light">
@@ -32,7 +39,15 @@ function Layout() {
             <Navbar.Brand className="logo">Rides</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle />
-          <Navbar.Collapse />
+          <Navbar.Collapse className="justify-content-end">
+            {
+              isLoggedIn && (
+                <Form>
+                  <Button type="button">Log out</Button>
+                </Form>
+              )
+            }
+          </Navbar.Collapse>
         </Container>
       </Navbar>
       <Container className="pt-3">
