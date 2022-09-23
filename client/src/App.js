f8fe49e0919b625b6e76699c1a541f530404521e
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-09-22 20:08:16 -0500

    Shows an alert on login error

--- client/src/App.js
+++ client/src/App.js
@@ -20,8 +20,10 @@ function App() {
       const response = await axios.post(url, { username, password });
       window.localStorage.setItem("rider.auth", JSON.stringify(response.data));
       setLoggedIn(true);
+      return {response, isError: false}
     } catch (error) {
       console.error(error);
+      return { response: error, isError: true}
     }
   };
 
