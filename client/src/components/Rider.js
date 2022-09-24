ad6125dccda969c0882bd02eb20a45e3afc71c4f
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-09-24 11:40:17 -0500

    rider only viewable by rider group

--- client/src/components/Rider.js
+++ client/src/components/Rider.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Breadcrumb, Card } from "react-bootstrap";
+import { Navigate } from "react-router-dom";
+
+import { isRider } from "../services/AuthService";
 
 function Rider(props) {
+  if (!isRider()) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <>
       <Breadcrumb>
@@ -10,15 +17,11 @@ function Rider(props) {
       </Breadcrumb>
       <Card className="mb-3">
         <Card.Header>Current Trip</Card.Header>
-        <Card.Body>
-            No trips.
-        </Card.Body>
+        <Card.Body>No trips.</Card.Body>
       </Card>
       <Card className="mb-3">
         <Card.Header>Recent Trips</Card.Header>
-        <Card.Body>
-            No trips.
-        </Card.Body>
+        <Card.Body>No trips.</Card.Body>
       </Card>
     </>
   );
