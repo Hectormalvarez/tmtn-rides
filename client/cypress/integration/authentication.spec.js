093ec56f32e8c99f1d68db97ab4edd949d037377
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-09-24 13:22:49 -0500

    added logIn Cypress refactor

--- client/cypress/integration/authentication.spec.js
+++ client/cypress/integration/authentication.spec.js
@@ -4,30 +4,19 @@ const email = faker.internet.email();
 const firstName = faker.name.firstName();
 const lastName = faker.name.lastName();
 
-const logIn = () => {
-  // Capture HTTP requests.
-  cy.intercept("POST", "log_in").as("logIn");
-  // Log into the app.
-  cy.visit("/#/log-in");
-  cy.get("input#username").type(email);
-  cy.get("input#password").type("pAssw0rd", { log: false });
-  cy.get("button").contains("Log in").click();
-  cy.wait("@logIn");
-};
-
 describe("Authentication", function () {
   it("Can sign up.", function () {
     cy.addUser(email, firstName, lastName, "rider")
   });
 
   it("Cannot visit the login page when logged in", function () {
-    logIn();
+    cy.logIn(email);
     cy.visit("/#/log-in");
     cy.hash().should("eq", "#/");
   });
 
   it("Can log out.", function () {
-    logIn();
+    cy.logIn(email);
     cy.get("button")
       .contains("Log out")
       .click()
@@ -63,13 +52,13 @@ describe("Authentication", function () {
   });
 
   it("Can log in.", function () {
-    logIn();
+    cy.logIn(email);
     cy.hash().should("eq", "#/");
     cy.get("button").contains("Log out");
   });
 
   it("Cannot see links when logged in.", function () {
-    logIn();
+    cy.logIn(email);
     cy.get('[href="#/sign-up"]').should("not.exist");
     cy.get('[href="#/sign-up"]').should("not.exist");
   });
